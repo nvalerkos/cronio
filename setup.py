a76f85637ddb14916c6c210c721c14abc66b938b
Nikolas Valerkos <n.valerkos@gmail.com>
2018-08-06 16:45:08 +0300

    Removed old license

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cronio",
+    version="0.0.1",
+    author="Nikolas Valerkos",
+    author_email="n.valerkos@gmail.com",
+    description="This project has a sender and a receiver, the sender sends commands through RabbitMQ on the queue of a worker (receiver), the receiver executes them either with OS or Python2.7",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nvalerkos/cronio",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
