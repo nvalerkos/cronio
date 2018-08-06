73bda92a6a377e0736d2913768e6254078067a6f
Nikolas Valerkos <31691447+nvalerkos@users.noreply.github.com>
2018-08-06 16:50:42 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     url="https://github.com/nvalerkos/cronio",
     packages=setuptools.find_packages(),
     classifiers=(
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
