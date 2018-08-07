1f4aeeb9c2aec1be00d9cd387d9a2d80eea5a7a9
Nikolas Valerkos <n.valerkos@gmail.com>
2018-08-07 16:23:39 +0300

    Modified setup.py to include required package stomp-py.

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
+import os
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 import setuptools
 
 with open("README.md", "r") as fh:
@@ -5,13 +11,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cronio",
-    version="1.0.0",
+    version="1.0.1",
     author="Nikolas Valerkos",
     author_email="n.valerkos@gmail.com",
     description="This project has a sender and a receiver, the sender sends commands through RabbitMQ on the queue of a worker (receiver), the receiver executes them either with OS or Python2.7",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/nvalerkos/cronio",
+    install_requires = required,
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 2",
